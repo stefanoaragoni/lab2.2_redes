bb0a00a0210c4ede8cb34ba6098403cf6c8b3a6c
stefanoaragoni <stefanoaragoni13@gmail.com>
2023-07-28 14:20:14 -0600

    DONE?

--- crc32_emisor.py
+++ crc32_emisor.py
@@ -19,47 +19,56 @@ def menu():
             break
 
     return trama
-    
+
 # P(x) = x^32 + x^26 + x^23 + x^22 + x^16 + x^12 + x^11 + x^10 + x^8 + x^7 + x^5 + x^4 + x^2 + x + 1
 def crc32(trama):
     # 11101101101110001000001100100000
-    polinomio = [int(bit) for bit in "11101101101110001000001100100000"]
+    polinomio = [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1]
+    SIZE = 32
     data = [int(bit) for bit in trama]  # Convert the trama to a list of integers
-    flag = False
 
     # Agregar ceros al final de la trama
-    data += [0] * 32
-    data_calc = []
+    data += [0] * SIZE
 
-    # Operacion XOR
-    while data != []:
-        
-        # Agregar valores de data a data_calc
-        if not (len(data_calc) >= len(polinomio)):
-            for i in range(len(polinomio) - len(data_calc)):
-                if data == []:
-                    break
-                else:
-                    a = data.pop(0)
-                    data_calc.append(a)
+    # Valores a utilizar
+    data_calc = data[0:len(polinomio)]
+    data = data[len(polinomio):]
 
-        # XOR entre los elementos de polinomio y trama
-        for i in range(len(polinomio)):
+    # polinomio | data
+    while True:
+        # Se eliminan los bits no significativos
+        eliminados = 0
+        while data_calc[0] == 0:
+            data_calc.pop(0)
+            eliminados += 1
 
-            if len(data_calc) == len(polinomio):
-                a = data_calc[i]
-                b = polinomio[i]
+        # Se agregan los bits a data_calc para que sea del mismo tamaño que el polinomio
+        for i in range(eliminados):
 
-                data_calc[i] = a ^ b
-            else:
+            # Si ya no hay bits en data, se termina el proceso
+            if len(data) == 0:
                 break
 
-        # Eliminar los primeros ceros de la trama
-        while data_calc[0] == 0 and data != []:
-            data_calc.pop(0)
+            else:
+                bit = data.pop(0)
+                data_calc.append(bit)
 
-    # Seleccionar los primeros 3 bits de data_calc
-    data_calc = data_calc[:32]
+        # Si datacalc es menor que el polinomio, se termina el proceso
+        if len(data_calc) < len(polinomio):
+            
+            # Si ya no hay bits en data, se termina el proceso
+            if len(data) == 0:
+                # Se agregan 0 al inicio de data_calc para que sea del mismo tamaño que el polinomio
+                for i in range(SIZE - len(data_calc)):
+                    data_calc.insert(0, 0)
+                break
+
+        # Si datacalc es igual al polinomio, se hace XOR entre ambos
+        if len(data_calc) == len(polinomio):
+            for i in range(len(polinomio)):
+                data_calc[i] ^= polinomio[i]
+            
+        print("XOR RESULT","".join([str(bit) for bit in data_calc]))
 
     # Convert the result back to a string representation
     crc = "".join([str(bit) for bit in data_calc])
---
+++ crc32_receptor.java
@@ -0,0 +1,81 @@
+// ░▒█▀▀▄░▒█▀▀▄░▒█▀▀▄░█▀▀█░█▀█░░░░░░░░▒█▀▀▄░▒█▀▀▀░▒█▀▀▄░▒█▀▀▀░▒█▀▀█░▀▀█▀▀░▒█▀▀▀█░▒█▀▀▄
+// ░▒█░░░░▒█▄▄▀░▒█░░░░░▒▀▄░▒▄▀░░░▀▀░░░▒█▄▄▀░▒█▀▀▀░▒█░░░░▒█▀▀▀░▒█▄▄█░░▒█░░░▒█░░▒█░▒█▄▄▀
+// ░▒█▄▄▀░▒█░▒█░▒█▄▄▀░█▄▄█░█▄▄░░░░░░░░▒█░▒█░▒█▄▄▄░▒█▄▄▀░▒█▄▄▄░▒█░░░░░▒█░░░▒█▄▄▄█░▒█░▒█
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class crc32_receptor {
+    public static String crc32(String trama) {
+        String polinomioStr = "111011011011100010000011001000001";
+        int SIZE = 32;
+
+        List<Integer> polinomio = new ArrayList<>();
+        for (char bit : polinomioStr.toCharArray()) {
+            polinomio.add(Character.getNumericValue(bit));
+        }
+
+        List<Integer> data = new ArrayList<>();
+        for (char bit : trama.toCharArray()) {
+            data.add(Character.getNumericValue(bit));
+        }
+
+        // Valores a utilizar
+        List<Integer> dataCalc = new ArrayList<>(data.subList(0, polinomio.size()));
+        data = new ArrayList<>(data.subList(polinomio.size(), data.size()));
+
+        // polinomio | data
+        while (true) {
+            // Se eliminan los bits no significativos
+            int eliminados = 0;
+            while (dataCalc.size() > 0 && dataCalc.get(0) == 0) {
+                dataCalc.remove(0);
+                eliminados++;
+            }
+
+            // Se agregan los bits a dataCalc para que sea del mismo tamaño que el polinomio
+            for (int i = 0; i < eliminados; i++) {
+                // Si ya no hay bits en data, se termina el proceso
+                if (data.isEmpty()) {
+                    break;
+                } else {
+                    int bit = data.remove(0);
+                    dataCalc.add(bit);
+                }
+            }
+
+            // Si dataCalc es menor que el polinomio, se termina el proceso
+            if (dataCalc.size() < polinomio.size()) {
+                // Si ya no hay bits en data, se termina el proceso
+                if (data.isEmpty()) {
+                    // Se agregan 0 al inicio de dataCalc para que sea del mismo tamaño que el polinomio
+                    while (dataCalc.size() < SIZE) {
+                        dataCalc.add(0, 0);
+                    }
+                    break;
+                }
+            }
+
+            // Si dataCalc es igual al polinomio, se hace XOR entre ambos
+            if (dataCalc.size() == polinomio.size()) {
+                for (int i = 0; i < polinomio.size(); i++) {
+                    dataCalc.set(i, dataCalc.get(i) ^ polinomio.get(i));
+                }
+            }
+        }
+
+        // Convert the result back to a string representation
+        StringBuilder crc = new StringBuilder();
+        for (int bit : dataCalc) {
+            crc.append(bit);
+        }
+
+        return crc.toString();
+    }
+
+    public static void main(String[] args) {
+        String trama = "11010110110101101010001111101000101000";
+        String encoded = crc32(trama);
+        System.out.println("\nTrama Modificada: "+trama+"\nError: " + encoded);
+    }
+}
